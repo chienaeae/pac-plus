207a010ee0d140c9dd025b7fa8e9fe2bbfe5fa32
chienaeae <chienaeae@gmail.com>
2024-05-12 15:03:43 -0400

    feat: add Resource header file

---
+++ include/game/Resource.h
@@ -0,0 +1,29 @@
+//
+// Created by mac on 2024/5/12.
+//
+
+#ifndef MAIN_RESOURCE_H
+#define MAIN_RESOURCE_H
+
+#include <string>
+
+class Resource {
+
+public:
+    Resource(): mIsOn(false) {}
+
+    virtual bool LoadFromFile (std::string) {
+        return true;
+    };
+
+    virtual void Free() {};
+
+    virtual bool isOn() {
+        return mIsOn;
+    };
+
+
+protected:
+    int mIsOn;
+};
+#endif //MAIN_RESOURCE_H
