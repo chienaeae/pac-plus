3ee5ed564df3e289987778a6879e1cbe059c4c4e
chienaeae <chienaeae@gmail.com>
2024-05-11 23:52:31 -0400

    feat: add base Object, Component

---
+++ include/game/Component.h
@@ -0,0 +1,34 @@
+//
+// Created by mac on 2024/5/11.
+//
+
+#ifndef MAIN_COMPONENT_H
+#define MAIN_COMPONENT_H
+
+#include "SDL.h"
+
+/* As our Object requires a reference to Component and Component
+requires a reference to Object; we forward declare Object */
+class Object;
+
+class Component {
+public:
+    explicit Component(Object *owner) : owner(owner){};
+
+    virtual void Awake(){};
+
+    virtual void Start(){};
+
+    virtual void Update(float deltaTime) {};
+
+    virtual void LateUpdate(float deltaTime ) {};
+
+    virtual void RenderUpdate(){};
+
+    virtual void EventUpdate(SDL_Event *e) {};
+
+protected:
+    Object * owner;
+};
+
+#endif //MAIN_COMPONENT_H
---
+++ include/game/Object.h
@@ -0,0 +1,68 @@
+//
+// Created by mac on 2024/5/11.
+//
+
+#ifndef MAIN_OBJECT_H
+#define MAIN_OBJECT_H
+
+#include <memory>
+#include <vector>
+#include <SDL.h>
+
+#include "game/Component.h"
+#include "game/ComponentTransform.h"
+
+class Object {
+public:
+    Object();
+
+    void Awake();
+
+    void Start();
+
+    void Update(float deltaTime);
+
+    void LateUpdate(float deltaTime);
+
+    void RenderUpdate();
+
+    void EventUpdate(SDL_Event *e);
+
+    template<typename T> std::shared_ptr<T> AddComponent(){
+        // This ensures that we only try to add a class the derives
+        // from Component. This is tested at compile time.
+        static_assert(std::is_base_of<Component, T>::value,
+                      "T must derive from Component");
+
+        for(auto& existingComponent: components) {
+            // Currently we prevent adding the same component twice.
+            // This may be something we will change in the future.
+            if(std::dynamic_pointer_cast<T>(existingComponent)){
+                return std::dynamic_pointer_cast<T>(existingComponent);
+            }
+        }
+
+        std::shared_ptr<T> newComponent = std::make_shared<T>(this);
+        components.push_back(newComponent);
+        return newComponent;
+    }
+
+    template<typename T> std::shared_ptr<T> GetComponent() {
+        static_assert(std::is_base_of<Component, T>::value,
+                      "T must derive from Component");
+
+        for(auto & existingComponent: components){
+            if(std::dynamic_pointer_cast<T>(existingComponent)){
+                return std::dynamic_pointer_cast<T>(existingComponent);
+            }
+        }
+
+        return nullptr;
+    }
+
+    std::shared_ptr<ComponentTransform> transform;
+private:
+    std::vector<std::shared_ptr<Component>> components;
+};
+
+#endif //MAIN_OBJECT_H
