acdb4bffddb054847e985f313842191e6ee4065a
chienaeae <chienaeae@gmail.com>
2024-05-11 21:28:52 -0400

    feat: add simple Sprite

---
+++ include/game/Sprite.h
@@ -0,0 +1,59 @@
+//
+// Created by mac on 2024/5/11.
+//
+
+#ifndef MAIN_SPRITE_H
+#define MAIN_SPRITE_H
+
+#include "game/LTexture.h"
+
+class Sprite {
+public:
+    Sprite();
+
+    const SDL_Rect& getTextureRect() const;
+
+    const LTexture* getTexture() const;
+
+    const int getPositionX() const;
+
+    const int getPositionY() const;
+
+    const float getScaleX() const;
+
+    const float getScaleY() const;
+
+    const int getRotation() const;
+
+    void setTextureRect(const SDL_Rect &tr);
+
+    void setTexture(const LTexture &t, bool resetRect=false);
+
+    void setPosition(int x, int y);
+
+    void move(int offsetX, int offsetY);
+
+    void setScale(float x, float y);
+
+    void scale(float factorX, float factorY);
+
+    void setRotation(int a);
+
+    void rotate(int a);
+
+    void render();
+private:
+    int posX;
+    int posY;
+
+    float scaleX;
+    float scaleY;
+
+    int angle;
+
+    SDL_Rect textureRect;
+
+    LTexture texture;
+};
+
+#endif //MAIN_SPRITE_H
